a92688f2354500405f9f3d350243d263354952d1
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-10-29 21:10:40 +0900

    Update button prop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <MyButton></MyButton>
+    <MyButton greet="Hello"></MyButton>
     <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
     <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
   </div>
