9ebc4d57c58150f6e38437eb0fc1d3efeb96b341
TakuyaTaniguchi <ttaniguchi@oikaze.jp>
2019-10-29 20:48:54 +0900

    firstCommit

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  lintOnSave: false
+  chainWebpack: config => {
+    config.module
+      .rule("eslint")
+      .use("eslint-loader")
+      .options({
+        fix: true
+      });
+  }
 };
